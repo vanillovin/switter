ff9be433298951e5a2dcd1c2479c0c8aea4b5832
vanillovin <vanillovin@gmail.com>
2022-07-31 14:52:32 +0900

    add useToggle custom hook

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+function useToggle(toggleRef) {
+  const [toggle, setToggle] = useState(false);
+
+  const onToggleChange = () => setToggle((prev) => !prev);
+
+  const clickToggleOutside = (event) => {
+    if (!toggleRef.current?.contains(event.target)) {
+      setToggle(false);
+    }
+  };
+
+  useEffect(() => {
+    if (toggle) {
+      document.addEventListener('mousedown', clickToggleOutside);
+    }
+    return () => document.addEventListener('mousedown', clickToggleOutside);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toggle]);
+
+  return [toggle, onToggleChange];
+}
+
+export default useToggle;
