ebd0c280236091cfdc01d9919a02fee18f62ac9f
vanillovin <vanillovin@gmail.com>
2022-08-03 22:16:29 +0900

    Add Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <div className="loading-component">
+      <div className="loading"></div>
+      <p>loading...</p>
+    </div>
+  );
+}
+
+export default Loading;
